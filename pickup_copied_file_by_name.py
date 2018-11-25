81f462a1d116ca2d4422d9d8bb2eda9940d0229a
TakayukiTomatsuri <TakayukiTomatsuri@users.noreply.github.com>
2018-11-25 16:14:12 +0900

    refactoring

--- pickup_copied_file_by_name.py
+++ pickup_copied_file_by_name.py
@@ -6,9 +6,11 @@ import sys
 import os
 import re
 
-IS_DEBUG = True
+def listup_dup(path, target_extentions, do_recursive):
+    target_extentions_regexp = [(".*" + item + "$") for item in target_extentions]
+    # insensiteve case
+    p_search_extention = re.compile("|".join(target_extentions_regexp), re.IGNORECASE)
 
-def listup_dup(path, p_search_extention):
     files = (file for file in os.listdir(path) if os.path.isfile( os.path.join(path, file) ) )
 
     photo_files = [file for file in files if p_search_extention.match(file)]
@@ -18,35 +20,18 @@ def listup_dup(path, p_search_extention):
         filename, ext = os.path.splitext(file)
         # extはデフォルトではドットつき！
         ext = ext.replace(".", "")
-        # if IS_DEBUG:
-        #     #print(filename + "\-[0-9]*" + "\." + ext)
-        #     print(file)
+
         dup_files.extend(  [file for file in photo_files if re.match(filename + "\-[0-9]*" + "\." + ext, file)] )
-
-
-
 
     dup_files_paths = [os.path.join(path, file) for file in dup_files]
 
-
-    if args.recursive:
+    if do_recursive:
         dires = (d for d in os.listdir(path) if not os.path.isfile( os.path.join(path, d) ) )
         for next_dir_name in dires:
             next_path = os.path.join(path, next_dir_name)
-            dup_files_paths.extend( listup_dup(next_path, p_search_extention) )
+            dup_files_paths.extend( listup_dup(next_path, target_extentions, do_recursive) )
 
     return dup_files_paths
-
-    # files = (file for file in os.listdir(path) if os.path.isfile( os.path.join(path, file) ) )
-
-    # photo_files = [file for file in files if p_search_extention.match(file)]
-
-    # probably_duplicated_files = [file for file in photo_files if re.match(".*-[0-9]*.*"  , file)]
-
-    # for file in probably_duplicated_files:
-    #     filename, ext = os.path.splitext(file)
-
-    #     # search original file
 
 
 if __name__ == '__main__':
@@ -55,21 +40,25 @@ if __name__ == '__main__':
 
     parser.add_argument('arg1', help='target dircotry.')
     parser.add_argument('-r', '--recursive', action="store_true", help='traverse directory recursively.')
+    parser.add_argument('-e', '--extension_list', nargs='+', default=[], help='target extentions.')
+    parser.add_argument('--debug', action="store_true", help='enable debug mode.')
     args = parser.parse_args()
 
     dir_target = args.arg1
     path_dir_target = Path(dir_target)
 
-    # no sensiteve cases
     extensions_tagert = [
-        ".*jpeg$",
-        ".*JPG$",
-        ".*RAF$"
+        "jpeg",
+        "JPG",
+        "RAF"
     ]
-    st = "|".join(extensions_tagert)
-    re_search_extention = re.compile(st, re.IGNORECASE)
 
-    for n in listup_dup(path_dir_target, re_search_extention):
+    extensions_tagert.extend(args.extension_list)
+
+    # st = "|".join(extensions_tagert)
+    # re_search_extention = re.compile(st, re.IGNORECASE)
+
+    for n in listup_dup(path_dir_target, extensions_tagert, args.debug):
         print(n)
 
 
